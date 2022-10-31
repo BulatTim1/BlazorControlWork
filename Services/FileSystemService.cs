b3bece4bd3c6cf8f802f3f59b20cc7347752bd19
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2022-10-31 12:15:20 +0300

    auth with uid in local storage, upload files with gridfs

---
+++ Services/FileSystemService.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System.Net.Sockets;
+
+namespace UserAuthBlazorApp.Services
+{
+    public class FileSystemService
+    {
+        static MongoClient client = new MongoClient("mongodb://localhost");
+        static IMongoDatabase database = client.GetDatabase("Images320");
+        static GridFSBucket gridFS = new GridFSBucket(database);
+        public void UploadImageToDb(string name)
+        {
+            using (FileStream fs = new FileStream("./Temp/" + name, FileMode.Open))
+            {
+                gridFS.UploadFromStream(name, fs);
+            }
+        }
+        public async Task UploadImageToDbAsync(string name, Stream fs)
+        {
+            await gridFS.UploadFromStreamAsync(name, fs);
+        }
+
+        public void DownloadToLocal(string name)
+        {
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory("./wwwroot/images/")}{name}", FileMode.Create))
+            {
+                gridFS.DownloadToStreamByName(name, fs);
+            }
+        }
+
+        public string[] GetAllNames()
+        {
+            var filter = Builders<GridFSFileInfo>.Filter.Empty;
+            string[] fileNames = new string[] { };
+            using (var cursor = gridFS.Find(filter))
+            {
+                fileNames = cursor.ToList().Select(x => x.Filename).ToArray();
+            }
+            return fileNames;
+        }
+
+        public void DeleteImage(string name)
+        {
+            var filter = Builders<GridFSFileInfo>.Filter.Eq("filename", name);
+            using (var cursor = gridFS.Find(filter))
+            {
+                var id = cursor.ToList().Select(x => x.Id).FirstOrDefault();
+                gridFS.Delete(id);
+            }
+        }
+    }
+}
---
+++ Services/MenuService.cs
@@ -0,0 +1,14 @@
+﻿using UserAuthBlazorApp.Data;
+
+namespace UserAuthBlazorApp.Services
+{
+    public class MenuService
+    {
+        public delegate void MenuAuthHandler();
+        public event MenuAuthHandler Notify;
+        public void NotifyEvent()
+        {
+            Notify();
+        }
+    }
+}
